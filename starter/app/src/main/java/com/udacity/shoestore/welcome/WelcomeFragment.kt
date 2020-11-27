0c42248f8c977fb2b4eb0a96e8ffb8b297b96bc8
Maicon Castro <maicondcastro@gmail.com>
2020-11-27 18:32:31 -0300

    Starting welcome fragment

---
+++ starter/app/src/main/java/com/udacity/shoestore/welcome/WelcomeFragment.kt
@@ -0,0 +1,28 @@
+package com.udacity.shoestore.welcome
+
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.udacity.shoestore.R
+
+class WelcomeFragment : Fragment() {
+
+    private lateinit var viewModel: WelcomeViewModel
+    
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.welcome_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(WelcomeViewModel::class.java)
+    }
+
+}
